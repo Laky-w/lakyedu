d912c2551c243dae00396eeca6a319e4bcfecaea
hero <502696885@qq.com>
2017-11-25 17:09:30 +0800

    登陆日志web层

---
+++ src/main/java/com/laky/edu/log/web/LoginLogController.java
@@ -0,0 +1,15 @@
+package com.laky.edu.log.web;
+
+import com.laky.edu.core.BaseController;
+import com.laky.edu.log.bean.LoginLog;
+import com.laky.edu.log.service.LoginLogService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log")
+public class LoginLogController extends BaseController{
+
+    
+}
