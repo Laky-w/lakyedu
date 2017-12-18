995c3bee2225e12f5a0e0420e98df797d54da839
hero <502696885@qq.com>
2017-12-19 00:28:41 +0800

    邀约试听接口增加,查询,service层对到访状态的判断,没毛病

---
+++ src/main/java/com/laky/edu/supply/bean/Invite.java
@@ -0,0 +1,90 @@
+package com.laky.edu.supply.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Invite implements Serializable {
+    private Integer id;
+
+    private Date inviteTime;
+
+    private Integer schoolZoneId;
+
+    private Integer studentId;
+
+    private Integer userId;
+
+    private Integer theStatus;
+
+    private Integer inviteStatus;
+
+    private String remarks;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSchoolZoneId() {
+        return schoolZoneId;
+    }
+
+    public void setSchoolZoneId(Integer schoolZoneId) {
+        this.schoolZoneId = schoolZoneId;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public Date getInviteTime() {
+        return inviteTime;
+    }
+
+    public void setInviteTime(Date inviteTime) {
+        this.inviteTime = inviteTime;
+    }
+
+    public Integer getTheStatus() {
+        return theStatus;
+    }
+
+    public void setTheStatus(Integer theStatus) {
+        this.theStatus = theStatus;
+    }
+
+    public Integer getInviteStatus() {
+        return inviteStatus;
+    }
+
+    public void setInviteStatus(Integer inviteStatus) {
+        this.inviteStatus = inviteStatus;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks == null ? null : remarks.trim();
+    }
+
+
+}
---
+++ src/main/java/com/laky/edu/supply/dao/InviteDao.java
@@ -0,0 +1,18 @@
+package com.laky.edu.supply.dao;
+
+import com.laky.edu.supply.bean.Invite;
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+@Component
+public interface InviteDao {
+
+    int insert(Invite invite);
+
+    List<Invite> selectByParameterMap(LinkedHashMap parameterMap);
+
+    int updateByPrimaryKeySelective(Invite record);
+
+    int updateByPrimaryKey(Invite record);
+}
